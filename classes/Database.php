c7f850754c1db79e7003d238659890890b384c32
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-17 12:08:25 +0200

    Updates 2021

--- classes/Database.php
+++ classes/Database.php
@@ -6,19 +6,22 @@ if (session_status() == PHP_SESSION_NONE) {
 
 class Database {
 
-  // private $host = "localhost"; // Host
-  // private $db_name = "cinematic"; // DB Name
-  // private $username = "root"; // DB Username
-  // private $password = ""; // DB Password
-  private $host = "sql313.epizy.com"; // Host
-  private $db_name = "epiz_23673363_cinematic"; // DB Name
-  private $username = "epiz_23673363"; // DB Username
-  private $password = "aUHYBpdhgfONW"; // DB Password
+  private $host = "localhost"; // Host
+  private $db_name = "cinematic"; // DB Name
+  private $username = "root"; // DB Username
+  private $password = ""; // DB Password
+
 
   private static $instance = null; // Instanca klase
   public $connection = null; // Konekcija
 
   private function __construct() {
+    if ($_SERVER['SERVER_NAME'] != "localhost") {
+      $this->host = $_ENV['dbhost']; // Host
+      $this->db_name = $_ENV['dbname']; // DB Name
+      $this->username = $_ENV['dbusername']; // DB Username
+      $this->password = $_ENV['dbpassword']; // DB Password    
+    }
     $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
   }
 
