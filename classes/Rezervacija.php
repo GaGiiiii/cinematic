3be1ec7cbad731ac4d75dfc23ef506d848b8f5ee
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-18 18:26:21 +0200

    Static function fix

--- classes/Rezervacija.php
+++ classes/Rezervacija.php
@@ -4,7 +4,7 @@ require_once 'Database.php';
 
 class Rezervacija {
 
-  public function dodaj($props) {
+  public static function dodaj($props) {
     $props = (object) $props;
     $query = "INSERT INTO rezervacija (korisnik_id, film_id, telefon, adresa, banka, datum, ime) VALUES ('$props->korisnik_id', '$props->film_id', '$props->brojTelefona', '$props->adresa', '$props->racun', '$props->datum', '$props->ime')";
     $result = mysqli_query(Database::getInstance()->getConnection(), $query) or die(mysqli_error(Database::getInstance()->getConnection()));
